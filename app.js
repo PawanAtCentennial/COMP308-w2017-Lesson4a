b439c8ed2482a13969ba0373f8195537cc8e5556
unknown <pvig@my.centennialcollege.ca>
2018-02-12 10:38:31 -0500

    bootstrap

--- app.js
+++ app.js
@@ -4,6 +4,39 @@ let favicon = require('serve-favicon');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
+//mongoose statement here
+
+let mongoose= require("mongoose");
+
+//mongoose uri
+
+let URI= "mongodb://thomas:thomas@ds046367.mlab.com:46367/games"
+
+mongoose.connect(URI,(err)=>{
+  if(err)
+  {
+    console.log("error connecting to db");
+  }
+  else{
+    console.log("connected to db");
+ 
+  }
+})
+
+
+//connect mongoose to mongo db and use games db
+/* mongoose.connect("mongodb://localhost/games",(err)=>{
+  if(err)
+  {
+    console.log("error connecting to db");
+  }
+  else{
+    console.log("connected to db");
+ 
+  }
+}) */
+
+
 
 let index = require('./routes/index');
 //let users = require('./routes/users');
